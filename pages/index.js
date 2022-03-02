2c793d65c3e22c6b70e4ea799df355cd56b9d0cb
Morgane Fanon <91192049+MorganeFanon@users.noreply.github.com>
2022-03-02 08:35:23 +0100

    ajout projet local dans repo Git

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import styled from 'styled-components';
+import phone from './Images/phone.jpg';
+
+export default function Home() {
+  return (
+    <Container>
+      <Head>
+      </Head>  
+        <div style={{width: '50%', textAlign: 'center'}}>
+        <Image src={phone} alt="whatsuuuup" height={250} width={250} />
+        <h2 style={{ color: '#727372' }}>Keep you  phone connected</h2>
+        <p style={{ color: '#b7b9bb' }}>
+          Whatssup connects you to sync messages. To reduce data usage, connect your phone to WI-FI.
+        </p>
+        </div>
+      </Container>
+  )
+}
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: grey;
+    width: 100%;
+    height: 100%;
+    `;
---
+++ styles/components/Sidebar.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Avatar } from '@mui/material';
+import turtle from '../../pages/Images/turtle.jpg';
+
+
+const Sidebar = () => {
+    return (
+        <Container>
+            <Header>
+               <UserAvatar src= {turtle} />
+            </Header>
+
+        </Container>
+    )
+}
+
+export default Sidebar
+
+const Container = styled.div`
+background-color: #FFFFFF;
+min-width: 320px;
+max-width: 450px;
+height: 100%;
+`;
+
+const Header = styled.div`
+display: flex;
+position: sticky;
+top: 0;
+background-color: white;
+justify-content: space-between;
+align-items: center;
+padding: 15px;
+height: 80px;
+border-bottom: 1px solid whitesmoke;
+width: 100%;
+`;
+
+const UserAvatar = styled(Avatar)`
+    cursor: pointer;
+    :hover{
+        opacity:0.8;
+    }
+`;
